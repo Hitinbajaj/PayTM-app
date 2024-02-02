ea53a49f963cb884aad37b13cc03f9190e674906
Hitinbajaj <hitinbajaj77@gmail.com>
2024-02-02 15:45:39 +0530

    config

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+const JWT_SECRET="hitin"
+
+
+const MONGO_URI= "mongodb+srv://hitinbajaj77:hitinbajaj77@cluster0.s5kelsz.mongodb.net/?retryWrites=true&w=majority"
+
+module.exports ={
+    JWT_SECRET,
+    MONGO_URI
+}
